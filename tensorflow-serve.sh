8878ebf4a44a379bed77498cf21c2e6cafdb7495
FabianBG <f4b4g3@gmail.com>
2019-07-16 20:41:00 -0500

    Clean project

--- tensorflow-serve.sh
+++ tensorflow-serve.sh
@@ -1,3 +1,3 @@
-docker run -t --rm -p 8501:8501 \
-    -v "/Users/latam/code/parca/models/:/models/" \
+docker run -t --rm -d -p 8501:8501 \
+    -v "/Users/mbastidas/code/parca/models/:/models/" \
     tensorflow/serving --model_config_file=/models/models.conf
